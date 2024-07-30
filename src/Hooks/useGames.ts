d2f41a1175e0be2cbe481f71d02618125ff7a65a
rajankit3107 <e22cseu0331@bennett.edu.in>
2024-07-30 09:51:02 +0530

    displaying platform icons

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../api/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform { //we creare interface to define the type of the data we are going to use.
+  id : number;
+  name: string;
+  slug : string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[];
   }
   
   interface FetchGamesResponse {
@@ -14,7 +21,7 @@ export interface Game {
   }
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
